9b1b5260a2b3d15d84ea34dca34e39899c4f8a6f
Haotian Yang <haotian.yang.cs@gmail.com>
2022-03-18 17:12:16 -0400

    implemented the basic function in project contract

--- contracts/Funding.sol
+++ contracts/Funding.sol
@@ -6,54 +6,110 @@ contract Manager{
     Project[] private projects;
 }
 
-contract Project {
+abstract contract Project{
 
+    // state stores all the infomation about for a project
     struct State{
         address owner;
-        address receiver_addr;
+        address receiver;
         string title;
         string description;
-        uint256 amount;
+        uint256 balance;
         uint256 goal_amount;
         uint256 blocks_num;
-        uint256 deadline_blocks_num;
-        mapping(address => int) addr_to_amount;
-        address[] sponser;
+        //TODO: maybe change deadline to a string with a physical clock? 
+        // From a user's perspective, using physical clock makes more sense.
+        uint256 deadline_blocks_num; 
+        mapping(address => uint256) contribution;
+        address[] funders;
+        bool active;
     }
+    State public state;
 
-    State public curr_state;
+    function contribute() public virtual payable returns(bool);
+    function changeDeadline(uint256) public virtual returns(bool);
+    function completeProject() public virtual returns(bool);
+    function cancelProject() public virtual returns(bool);
+    function getCurrentState() public virtual;
 
 
 }
 
 contract ProjectStandard is Project{
 
-   constructor(address owner, 
-               address receiver_addr, 
+    event stateInfo(address, address, string,  string, uint256, uint256, uint256, uint256, address[]);
+    event fundingRecevied(address, uint256, uint256);
+    event projectCompleted(address, address, uint256);
+    event projectCanceled(address, address, uint256);
+
+    constructor(address owner, 
+               address receiver, 
                string memory title, 
                string memory description,
                uint256 goal_amount,
                uint256 deadline_blocks_num) {
-        curr_state.owner = owner;
-        curr_state.receiver_addr = receiver_addr;
-        curr_state.title = title;
-        curr_state.description = description;
-        curr_state.goal_amount = goal_amount;
-        curr_state.deadline_blocks_num = deadline_blocks_num;
-        curr_state.amount = 0;
-        curr_state.blocks_num = 0;
+        state.owner = owner;
+        state.receiver = receiver;
+        state.title = title;
+        state.description = description;
+        state.goal_amount = goal_amount;
+        state.deadline_blocks_num = deadline_blocks_num;
+        state.balance = 0;
+        state.blocks_num = 0;
+        state.active = true;
     }
 
-    function getCurrentState() public view returns(address, address, string memory, string memory, uint256, uint256, uint256, uint256, address[] memory) {
-        return (curr_state.owner,
-                curr_state.receiver_addr,
-                curr_state.title,
-                curr_state.description,
-                curr_state.amount,
-                curr_state.goal_amount,
-                curr_state.blocks_num,
-                curr_state.deadline_blocks_num,
-                curr_state.sponser);
+    function contribute() public override payable returns(bool){
+        require(state.active, "This project is not active!");
+        state.balance += msg.value;
+        
+        // record the funder if it's the first time contribute this project
+        if(state.contribution[msg.sender] == 0){
+            state.funders.push(msg.sender);
+        }
+
+        state.contribution[msg.sender] += msg.value;
+        emit fundingRecevied(msg.sender, msg.value, state.balance);
+        return true;
+    }
+
+    function completeProject() public override returns(bool){
+        require(state.active, "This project is no longer active!");
+        payable(state.receiver).transfer(address(this).balance);
+        state.active = false;
+        emit projectCompleted(state.owner, state.receiver, state.balance);
+        return true;
+    }
+
+    function cancelProject() public override returns(bool){
+        require(state.active, "This project is no longer active!");
+        for(uint i = 0; i < state.funders.length; i++){
+            address funder = state.funders[i];
+            uint256 contribution = state.contribution[funder];
+            // refund the contribution
+            payable(funder).transfer(contribution);
+            state.balance -= contribution;
+        }
+        state.active = false;
+        emit projectCanceled(state.owner, state.receiver, state.balance);
+        return true;
+    }
+
+    function changeDeadline(uint256) public override returns(bool){
+        return true;
+    }
+
+    // TODO change it events
+    function getCurrentState() public override {
+        emit stateInfo(state.owner,
+                state.receiver,
+                state.title,
+                state.description,
+                state.balance,
+                state.goal_amount,
+                state.blocks_num,
+                state.deadline_blocks_num,
+                state.funders);
     }
 
 }
