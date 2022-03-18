47191e3e2f1c847d00f3487589f0a4790ef7dbbf
zchen <chenzze1@student.ubc.ca>
2022-03-18 16:20:22 -0700

    add modifier and minor fix in contract

--- contracts/Funding.sol
+++ contracts/Funding.sol
@@ -14,20 +14,36 @@ abstract contract Project{
         address receiver;
         string title;
         string description;
-        uint256 balance;
+        string img_url;
+        uint256 amount;
         uint256 goal_amount;
-        uint256 blocks_num;
+        uint blocks_num;
         //TODO: maybe change deadline to a string with a physical clock? 
         // From a user's perspective, using physical clock makes more sense.
-        uint256 deadline_blocks_num; 
+        uint deadline; 
         mapping(address => uint256) contribution;
         address[] funders;
         bool active;
     }
     State public state;
 
+    modifier ensure() {
+        require(state.deadline >= block.timestamp, "This project is expired!");
+        _;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == state.owner, "Permission denied.");
+        _;
+    }
+
+    modifier active() {
+        require(state.active, "This project is not active!");
+        _;
+    }
+
     function contribute() public virtual payable returns(bool);
-    function changeDeadline(uint256) public virtual returns(bool);
+    function changeDeadline(uint) public virtual returns(bool);
     function completeProject() public virtual returns(bool);
     function cancelProject() public virtual returns(bool);
     function getCurrentState() public virtual;
@@ -37,31 +53,32 @@ abstract contract Project{
 
 contract ProjectStandard is Project{
 
-    event stateInfo(address, address, string,  string, uint256, uint256, uint256, uint256, address[]);
-    event fundingRecevied(address, uint256, uint256);
-    event projectCompleted(address, address, uint256);
-    event projectCanceled(address, address, uint256);
+    event stateInfo(address indexed, address indexed, string, string, string, uint256, uint256, uint256, uint, uint, address[]);
+    event fundingRecevied(address indexed, uint256, uint256);
+    event projectCompleted(address indexed, address indexed, uint256);
+    event projectCanceled(address indexed, address indexed, uint256);
 
     constructor(address owner, 
                address receiver, 
                string memory title, 
                string memory description,
+               string memory img_url,
                uint256 goal_amount,
-               uint256 deadline_blocks_num) {
+               uint deadline_blocks_num) {
         state.owner = owner;
         state.receiver = receiver;
         state.title = title;
         state.description = description;
+        state.img_url = img_url;
         state.goal_amount = goal_amount;
-        state.deadline_blocks_num = deadline_blocks_num;
-        state.balance = 0;
-        state.blocks_num = 0;
+        state.deadline = deadline_blocks_num;
+        state.amount = 0;
+        state.blocks_num = block.timestamp;
         state.active = true;
     }
 
-    function contribute() public override payable returns(bool){
-        require(state.active, "This project is not active!");
-        state.balance += msg.value;
+    function contribute() public active ensure override payable returns(bool){
+        state.amount += msg.value;
         
         // record the funder if it's the first time contribute this project
         if(state.contribution[msg.sender] == 0){
@@ -69,33 +86,32 @@ contract ProjectStandard is Project{
         }
 
         state.contribution[msg.sender] += msg.value;
-        emit fundingRecevied(msg.sender, msg.value, state.balance);
+        emit fundingRecevied(msg.sender, msg.value, address(this).balance);
         return true;
     }
 
-    function completeProject() public override returns(bool){
-        require(state.active, "This project is no longer active!");
+    function completeProject() public active onlyOwner override returns(bool){
         payable(state.receiver).transfer(address(this).balance);
         state.active = false;
-        emit projectCompleted(state.owner, state.receiver, state.balance);
+        emit projectCompleted(state.owner, state.receiver, address(this).balance);
         return true;
     }
 
-    function cancelProject() public override returns(bool){
-        require(state.active, "This project is no longer active!");
+    function cancelProject() public active onlyOwner override returns(bool){
         for(uint i = 0; i < state.funders.length; i++){
             address funder = state.funders[i];
             uint256 contribution = state.contribution[funder];
             // refund the contribution
             payable(funder).transfer(contribution);
-            state.balance -= contribution;
         }
         state.active = false;
-        emit projectCanceled(state.owner, state.receiver, state.balance);
+        emit projectCanceled(state.owner, state.receiver, address(this).balance);
         return true;
     }
 
-    function changeDeadline(uint256) public override returns(bool){
+    function changeDeadline(uint _deadline) public ensure active onlyOwner override returns(bool){
+        require(_deadline > state.deadline, "New deadline should be later than old deadline.");
+        state.deadline = _deadline;
         return true;
     }
 
@@ -105,10 +121,12 @@ contract ProjectStandard is Project{
                 state.receiver,
                 state.title,
                 state.description,
-                state.balance,
+                state.img_url,
+                state.amount,
                 state.goal_amount,
+                address(this).balance,
                 state.blocks_num,
-                state.deadline_blocks_num,
+                state.deadline,
                 state.funders);
     }
 
