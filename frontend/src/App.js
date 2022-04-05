b7f27d01e0bb9de194b23722deccd743696e74de
Estelle Qiu <44105167+xqiu1@users.noreply.github.com>
2022-04-04 18:02:46 -0700

    frontend: tsx to js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Homepage } from "./components/Homepage";
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
