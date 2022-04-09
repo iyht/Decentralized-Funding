e7582e1ba1f849e3b079e3e83c11d04f2e09e4b7
Estelle Qiu <44105167+xqiu1@users.noreply.github.com>
2022-04-08 17:41:12 -0700

    frontend: refactor routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,10 @@
 import "./App.css";
-import { useState, useEffect, createContext } from "react";
+import { useState, createContext } from "react";
 import { Layout, Menu } from "antd";
-import { useWeb3React } from "@web3-react/core";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import Web3 from "web3";
 
 import UBClogo from "./ubc-logo.png";
 import { NavRoutes } from "./components/routes";
-
-import ManagerArtifact from "./artifacts/contracts/Funding.sol/Manager.json";
-
-export const CONTACT_ADDRESS = "0xD3d1f12dC6a31Cd2b66CD10C6583fbF46D1bC701";
 
 const { Header, Content, Footer } = Layout;
 
@@ -36,80 +30,6 @@ function App() {
   const [currentMenuItem, setCurrentMenuItem] = useState(
     window.location.pathname
   );
-  const context = useWeb3React();
-  const { account, library, active } = context;
-
-  const [signer, setSigner] = useState();
-  const [fundingContract, setFundingContract] = useState();
-  const [fundingContractAddr, setFundingContractAddr] = useState("");
-
-  useEffect(() => {
-    // async function getWeb3Provider() {
-    //   if (window.ethereum) {
-    //     window.web3 = new Web3(window.ethereum);
-    //     await window.ethereum.enable();
-    //   } else if (window.web3) {
-    //     window.web3 = new Web3(window.web3.currentProvider);
-    //   } else {
-    //     window.alert(
-    //       "Non-Ethereum browser detected. You should consider trying MetaMask!"
-    //     );
-    //   }
-    // }
-
-    // async function load() {
-    //   await getWeb3Provider();
-    //   const accounts = await window.web3.eth.getAccounts();
-    //   console.log("Attempting to deploy from account", accounts[0]);
-    //   const myAccount = accounts[0];
-    //   console.log(myAccount);
-    //   // Instantiate smart contract using ABI and address.
-    //   const ManagerContract = new window.web3.eth.Contract(
-    //     ManagerArtifact.abi,
-    //     CONTACT_ADDRESS
-    //   );
-    //   console.log({ ManagerContract });
-    // }
-
-    // load();
-
-    if (typeof account === "undefined" || account === null || !library) {
-      return;
-    }
-
-    console.log(account);
-  }, [account]);
-
-  // useEffect(() => {
-  //   if (!library) {
-  //     setSigner(undefined);
-  //     return;
-  //   }
-  //   console.log(library.getSigner());
-  //   setSigner(library.getSigner());
-  // }, [library]);
-
-  // const _contract = handleDeployContract(event, signer, fundingContract);
-  // if (_contract !== fundingContract) {
-  //   setFundingContract(_contract);
-  //   console.log(_contract.getAllProjects());
-  // }
-
-  // useEffect(() => {
-  //   if (!fundingContract) {
-  //     return;
-  //   }
-
-  //   async function getFunding(fundingContract) {
-  //     const _projects = await fundingContract.getAllProjects();
-
-  //     if (_projects !== projects) {
-  //       setGreeting(_projects);
-  //     }
-  //   }
-
-  //   getFunding(fundingContract);
-  // }, [fundingContract, projects]);
 
   const handleClickMenuItem = (e) => {
     setCurrentMenuItem(e.key);
@@ -120,7 +40,8 @@ function App() {
       <Router>
         <Layout style={{ minHeight: "100vh" }}>
           <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
-            <Menu theme="dark"
+            <Menu
+              theme="dark"
               mode="horizontal"
               onClick={handleClickMenuItem}
               selectedKeys={[currentMenuItem]}
@@ -136,16 +57,12 @@ function App() {
             className="site-layout"
             style={{ padding: "0 50px", marginTop: 64 }}
           >
-            <ContractContext.Provider
-              value={{ signer, setSigner, fundingContract, setFundingContract }}
+            <div
+              className="site-layout-background"
+              style={{ padding: 24, minHeight: 380 }}
             >
-              <div
-                className="site-layout-background"
-                style={{ padding: 24, minHeight: 380 }}
-              >
-                <NavRoutes />
-              </div>
-            </ContractContext.Provider>
+              <NavRoutes />
+            </div>
           </Content>
           <Footer style={{ textAlign: "center" }}>
             <img
