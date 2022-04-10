9094894719acf009e07535a58133c64a03dd1dcc
Haotian Yang <haotian.yang.cs@gmail.com>
2022-04-10 08:36:40 +0800

    upload image but don't have http server

--- frontend/src/components/create-project.js
+++ frontend/src/components/create-project.js
@@ -18,7 +18,13 @@ import {
   InputNumber,
   TreeSelect,
   Switch,
+  message,
+  Upload
 } from 'antd';
+
+import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
+
+
 
 import { Contract, ethers, Signer } from 'ethers';
 
@@ -30,7 +36,76 @@ const ManagerInfo = {
 }
 
 
+
 const { Title } = Typography;
+
+function getBase64(img, callback) {
+  const reader = new FileReader();
+  reader.addEventListener('load', () => callback(reader.result));
+  reader.readAsDataURL(img);
+}
+
+function beforeUpload(file) {
+  const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+  if (!isJpgOrPng) {
+    message.error('You can only upload JPG/PNG file!');
+  }
+  const isLt2M = file.size / 1024 / 1024 < 2;
+  if (!isLt2M) {
+    message.error('Image must smaller than 2MB!');
+  }
+  return isJpgOrPng && isLt2M;
+}
+
+class Avatar extends React.Component {
+  state = {
+    loading: false,
+  };
+
+  handleChange = info => {
+    if (info.file.status === 'uploading') {
+      this.setState({ loading: true });
+      return;
+    }
+    if (info.file.status === 'done') {
+      // Get this url from response in real world.
+      getBase64(info.file.originFileObj, imageUrl =>
+        this.setState({
+          imageUrl,
+          loading: false,
+        }),
+      );
+    }
+  };
+
+  LoadingOutlined
+
+  render() {
+    const { loading, imageUrl } = this.state;
+    const uploadButton = (
+      <div>
+        {loading ? <LoadingOutlined /> : <PlusOutlined />}
+        <div style={{ marginTop: 8 }}>Upload</div>
+      </div>
+    );
+    return (
+      <Upload
+        name="avatar"
+        listType="picture-card"
+        className="avatar-uploader"
+        showUploadList={false}
+        action="//jsonplaceholder.typicode.com/posts/"
+        beforeUpload={beforeUpload}
+        onChange={this.handleChange}
+      >
+        {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
+      </Upload>
+    );
+  }
+}
+
+
+
 
 
 const ProjectForm = () => {
@@ -44,7 +119,7 @@ const ProjectForm = () => {
     await provider.send("eth_requestAccounts", []);
     const signer = provider.getSigner();
     let userAddress = await signer.getAddress();
-    
+
     // get the contract instance
     const manager = new ethers.Contract(ManagerInfo.address, ManagerInfo.abi, signer);
 
@@ -86,29 +161,29 @@ const ProjectForm = () => {
   }
 
 
-  return ( 
-      <Form 
+  return (
+    <Form
       form={form}
       name="Project"
       onFinish={onFinish}
-       >
-        <Form.Item name="title" label="Title" rules={[ { required: true }, ]} >
-          <Input />
-        </Form.Item>
-        <Form.Item name="description" label="Description" rules={[ { required: true }, ]} >
-          <Input />
-        </Form.Item>
-        <Form.Item name="goal_amount" label="Goal Amount" rules={[ { required: true }, ]} >
-          <InputNumber/>
-        </Form.Item>
-        <Form.Item label="Due Date" name="due_date">
-          <DatePicker />
-        </Form.Item>
-        <Form.Item >
-          <Button type="primary" htmlType="submit">Submit</Button>
-        </Form.Item> 
-      </Form>)
-    
+    >
+      <Form.Item name="title" label="Title" rules={[{ required: true },]} >
+        <Input />
+      </Form.Item>
+      <Form.Item name="description" label="Description" rules={[{ required: true },]} >
+        <Input />
+      </Form.Item>
+      <Form.Item name="goal_amount" label="Goal Amount(ethers)" rules={[{ required: true },]} >
+        <InputNumber />
+      </Form.Item>
+      <Form.Item name="duration" label="Duration(days)" rules={[{ required: true },]} >
+        <InputNumber />
+      </Form.Item>
+      <Form.Item >
+        <Button type="primary" htmlType="submit">Submit</Button>
+      </Form.Item>
+    </Form>)
+
 };
 
 
@@ -118,7 +193,8 @@ export const CreateProject = () => {
   return (
     <div>
       <Title level={2}>Create your project now</Title>
-      <ProjectForm/>
+      <ProjectForm />
+      {/* <Avatar /> */}
     </div>
   );
 };
