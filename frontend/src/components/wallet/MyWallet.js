c47e9ad6654c0aa0e98b5bca338d83eddb6137f5
Estelle Qiu <44105167+xqiu1@users.noreply.github.com>
2022-04-11 11:13:16 -0700

    frontend: hide wallet

--- frontend/src/components/wallet/MyWallet.js
+++ frontend/src/components/wallet/MyWallet.js
@@ -1,32 +1,33 @@
+import { useState } from "react";
+import { Affix, Button, Space, Popover } from "antd";
+import { WalletTwoTone } from "@ant-design/icons";
+import {
+  NoEthereumProviderError,
+  UserRejectedRequestError,
+} from "@web3-react/injected-connector";
+import { UnsupportedChainIdError, useWeb3React } from "@web3-react/core";
+import { useEagerConnect, useInactiveListener } from "./hooks";
 import { WalletStatus } from "./WalletState";
 import { injected } from "./connectors";
-import { useState } from 'react';
-import {
-    NoEthereumProviderError,
-    UserRejectedRequestError
-  } from '@web3-react/injected-connector';
-import { UnsupportedChainIdError, useWeb3React } from '@web3-react/core';
-import { Button } from "antd";
-import { useEagerConnect, useInactiveListener } from "./hooks";
 
-function Connect(){
-    const context = useWeb3React();
-    const { activate, active } = context;
-  
-    const [activating, setActivating] = useState(false);
-  
-    function handleActivate(event){
-      event.preventDefault();
-  
-      async function _activate(activate){
-        setActivating(true);
-        await activate(injected);
-        setActivating(false);
-      }
-  
-      _activate(activate);
+function Connect() {
+  const context = useWeb3React();
+  const { activate, active } = context;
+
+  const [activating, setActivating] = useState(false);
+
+  function handleActivate(event) {
+    event.preventDefault();
+
+    async function _activate(activate) {
+      setActivating(true);
+      await activate(injected);
+      setActivating(false);
     }
-    // handle logic to eagerly connect to the injected ethereum provider, if it exists and has
+
+    _activate(activate);
+  }
+  // handle logic to eagerly connect to the injected ethereum provider, if it exists and has
   // granted access already
   const eagerConnectionSuccessful = useEagerConnect();
 
@@ -38,88 +39,104 @@ function Connect(){
     <Button
       disabled={active}
       style={{
-        cursor: active ? 'not-allowed' : 'pointer',
-        borderColor: activating ? 'orange' : active ? 'unset' : 'green'
+        cursor: active ? "not-allowed" : "pointer",
+        borderColor: activating ? "orange" : active ? "unset" : "green",
       }}
       type="primary"
       size="large"
       shape="round"
       onClick={handleActivate}
     >
-    🦊 Connect
+      🦊 Connect
     </Button>
   );
 }
 
-function Disconnect(){
-    const context = useWeb3React();
-    const { deactivate, active } = context;
-  
-    function handleDeactivate(event){
-      event.preventDefault();
-      deactivate();
-    }
-  
-    return (
-      <Button
-        disabled={!active}
-        style={{
-          cursor: active ? 'pointer' : 'not-allowed',
-          borderColor: active ? 'red' : 'unset'
-        }}
-        onClick={handleDeactivate}
-        type="primary"
-        size="large"
-        shape="round"
+function Disconnect() {
+  const context = useWeb3React();
+  const { deactivate, active } = context;
+
+  function handleDeactivate(event) {
+    event.preventDefault();
+    deactivate();
+  }
+
+  return (
+    <Button
+      disabled={!active}
+      style={{
+        cursor: active ? "pointer" : "not-allowed",
+      }}
+      onClick={handleDeactivate}
+      type="primary"
+      size="large"
+      shape="round"
+    >
+      Disconnect
+    </Button>
+  );
+}
+
+function getErrorMessage(error) {
+  let errorMessage;
+
+  switch (error.constructor) {
+    case NoEthereumProviderError:
+      errorMessage = `No Ethereum browser extension detected. Please install MetaMask extension.`;
+      break;
+    case UnsupportedChainIdError:
+      errorMessage = `You're connected to an unsupported network.`;
+      break;
+    case UserRejectedRequestError:
+      errorMessage = `Please authorize this website to access your Ethereum account.`;
+      break;
+    default:
+      errorMessage = error.message;
+  }
+
+  return errorMessage;
+}
+
+export function ConnectionModule() {
+  const context = useWeb3React();
+  const { error } = context;
+
+  if (!!error) {
+    window.alert(getErrorMessage(error));
+  }
+
+  return (
+    <Space style={{ marginTop: 24 }}>
+      <Connect />
+      <Disconnect />
+    </Space>
+  );
+}
+
+function MyWallet() {
+  const [visible, setVisible] = useState(false);
+
+  const handleVisibleChange = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <Affix style={{ position: "fixed", bottom: 60, right: 60 }}>
+      <Popover
+        content={
+          <div>
+            <WalletStatus />
+            <ConnectionModule />
+          </div>
+        }
+        trigger="click"
+        visible={visible}
+        onVisibleChange={handleVisibleChange}
       >
-        Disconnect
-      </Button>
-    );
-  }
-
-function getErrorMessage(error){
-    let errorMessage;
-  
-    switch (error.constructor) {
-      case NoEthereumProviderError:
-        errorMessage = `No Ethereum browser extension detected. Please install MetaMask extension.`;
-        break;
-      case UnsupportedChainIdError:
-        errorMessage = `You're connected to an unsupported network.`;
-        break;
-      case UserRejectedRequestError:
-        errorMessage = `Please authorize this website to access your Ethereum account.`;
-        break;
-      default:
-        errorMessage = error.message;
-    }
-  
-    return errorMessage;
-  }
-
-export function ConnectionModule(){
-    const context = useWeb3React();
-    const { error } = context;
-  
-    if (!!error) {
-      window.alert(getErrorMessage(error));
-    }
-  
-    return (
-        <div>
-            <Connect />
-            <Disconnect />
-        </div>
-    );
-  }
-
-
-function MyWallet(props){
-
-    return <div>
-          <WalletStatus/>
-          <ConnectionModule/>
-    </div>
+        <WalletTwoTone twoToneColor="#1890ff" style={{ fontSize: 36 }} />
+      </Popover>
+    </Affix>
+  );
 }
 
 export default MyWallet;
