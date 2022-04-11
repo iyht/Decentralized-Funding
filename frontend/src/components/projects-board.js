bdca60406192c05987ca3a5ebccd817b3540e1ec
Haotian Yang <haotian.yang.cs@gmail.com>
2022-04-11 15:26:30 +0800

    refactor project page

--- frontend/src/components/projects-board.js
+++ frontend/src/components/projects-board.js
@@ -1,35 +1,23 @@
 import { ethers } from "ethers";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import _ from "lodash";
 
-import { ManagerInfo } from "./config/artifacts";
 import { ProjectList } from "./projects/project-list";
+import { ContractContext } from "./utils/contract_context";
 
 export const ProjectsBoard = ({}) => {
   const [projects, setProjects] = useState([]);
+  const { manager, provider, signer, setManager, setProvider, setSigner } = useContext(ContractContext);
 
   useEffect(() => {
     async function getManager() {
-      const provider = new ethers.providers.Web3Provider(
-        window.ethereum,
-        "any"
-      );
-      await provider.send("eth_requestAccounts", []);
-      const signer = provider.getSigner();
-
-      const manager = new ethers.Contract(
-        ManagerInfo.address,
-        ManagerInfo.abi,
-        signer
-      );
-
       const _projects = await manager.getAllProjects();
       if (!_.isEqual(projects, _projects)) {
         setProjects(_projects);
       }
     }
     getManager();
-  }, [projects]);
+  }, [manager]);
 
   return <ProjectList projects={projects} />;
 };
